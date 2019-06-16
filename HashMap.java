17d5140935197fdf074dab07ed3dc59d77da73ad
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-16 21:30:18 +0300

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -1,1 +1,11 @@
+package com.company.newMap;
+
+public interface HashMap<K, V> {
+    public void put(K key, V value);
+    public V get(K key);
+    void increase();
+    public void clear();
+    public boolean isEmpty();
+    public int size();
+}
 
